b32de2c7254184f60e7888e3e2dd3880e2045802
Chris Taylor <chris@christaylordeveloper.co.uk>
2022-12-17 12:50:01 +0100

    Add placeholder javadoc.

--- cucumber/hellocucumber/src/test/java/hellocucumber/ExampleSteps.java
+++ cucumber/hellocucumber/src/test/java/hellocucumber/ExampleSteps.java
@@ -13,30 +13,53 @@ import org.junit.Assert;
 
 public class ExampleSteps {
 
+    /**
+     * Put javadoc here.
+     */
     private HomePage homePage;
 
+    /**
+     * Put javadoc here.
+     */
     private WebDriver driver = new RemoteWebDriver(
         new URL("http://chrome:4444/wd/hub"), new ChromeOptions()
     );
 
+    /**
+     * Put javadoc here.
+     */
     public ExampleSteps() throws MalformedURLException { }
 
+    /**
+     * Put javadoc here.
+     */
     @Given("I am on our application Home Page")
     public void iVisitHomepage() {
         driver.get("http://flask");
         this.homePage = new HomePage(driver);
     }
 
+    /**
+     * Put javadoc here.
+     * @param buttonId
+     */
     @When("I press the {string} button")
     public void pressButton(String buttonId) {
         this.homePage.getShowButton().click();
     }
 
+    /**
+     * Put javadoc here.
+     * @param coloursList
+     */
     @Then("I sould see colours list {string}")
     public void assertColoursList(String coloursList) {
         Assert.assertEquals(coloursList, this.homePage.getColoursList());
     }
 
+    /**
+     * Put javadoc here.
+     */
     @After()
     public void closeBrowser() {
         driver.quit();
--- cucumber/hellocucumber/src/test/java/hellocucumber/HomePage.java
+++ cucumber/hellocucumber/src/test/java/hellocucumber/HomePage.java
@@ -1,3 +1,6 @@
+/**
+ * Put javadoc here.
+ */
 package hellocucumber;
 
 import java.util.List;
@@ -11,13 +14,35 @@ import org.openqa.selenium.WebElement;
 
 public class HomePage {
 
+    /**
+     * Put javadoc here.
+     */
     protected final WebDriver driver;
 
+    /**
+     * Put javadoc here.
+     */
     private final String titleStart = "bdd";
+
+    /**
+     * Put javadoc here.
+     */
     private final By headerBy = By.tagName("h1");
+
+    /**
+     * Put javadoc here.
+     */
     private final By showButtonBy = By.id("show");
+
+    /**
+     * Put javadoc here.
+     */
     private final By colourListBy = By.id("colours-list");
 
+    /**
+     * Put javadoc here.
+     * @param driver
+     */
     public HomePage(WebDriver driver) {
         this.driver = driver;
         Duration fiveSecs = Duration.ofSeconds(5);
@@ -31,16 +56,28 @@ public class HomePage {
         );
     }
 
+    /**
+     * Put javadoc here.
+     * @return tbc
+     */
     public WebElement getShowButton() {
         return driver.findElement(showButtonBy);
     }
 
+    /**
+     * Put javadoc here.
+     * @return tbc
+     */
     public String getHeaderMessage() {
         List<WebElement> headers = driver.findElements(headerBy);
         Assert.assertEquals("Headers", 1, headers.size());
         return headers.get(0).getText();
     }
 
+    /**
+     * Put javadoc here.
+     * @return tbc
+     */
     public String getColoursList() {
         return driver.findElement(colourListBy).getText();
     }
